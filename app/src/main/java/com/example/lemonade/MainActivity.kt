7e8a25720d2be072b37d4cbe19d8f1528a13f338
tunjiarofin <tunjiarofin@gmail.com>
2025-02-19 04:59:07 +0100

    Tough with few errors here and there. but gradually getting there

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -15,15 +15,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonDefaults.shape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,24 +57,87 @@ fun LemonadeApp(modifier: Modifier = Modifier
     .wrapContentSize(Alignment.Center)
 ){
 
+    var result by remember { mutableStateOf(1) }
+
+    var changePage = when(result){
+        1 -> Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(180.dp)
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = "Lemon Tree",
+            )
+
+        }
+
+        2 -> Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(180.dp)
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.lemon_squeeze),
+                contentDescription = "Lemon Tree",
+            )
+
+        }
+
+        3 -> Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(180.dp)
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.lemon_drink),
+                contentDescription = "Lemon Tree",
+            )
+
+        }
+
+        else -> Button(onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(180.dp)
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.lemon_restart),
+                contentDescription = "Lemon Tree",
+            )
+
+        }
+    }
+
+
     Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
 
-
-
-
     ){
-        Image(
-            painter = painterResource(R.drawable.lemon_tree),
-            contentDescription = "Lemon Tree",
-            modifier = Modifier
-                .align(alignment = Alignment.CenterHorizontally)
-                .size(180.dp)
-                .background(Color.Yellow, shape = RoundedCornerShape(30.dp))
+        Button(onClick = { result = 1..4},
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow),
+            shape = RoundedCornerShape(30.dp),
+            modifier = Modifier.size(180.dp)
+        ) {
+
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = "Lemon Tree",
+            )
+
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.lemon_tree) //The text Below
 
         )
+
     }
 
     }
